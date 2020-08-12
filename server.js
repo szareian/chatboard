b1c5f498a2b1d945c14cb2446176f314f3fc8932
szareian <szareian@sfu.ca>
2020-08-12 12:11:52 -0700

    Added functionality so other users can join/leave the video chat

--- server.js
+++ server.js
@@ -20,7 +20,10 @@ io.on('connection', socket => {
     socket.on('join-room', (roomId, userId) => {
         socket.join(roomId)
         socket.to(roomId).broadcast.emit('user-connected', userId)
-        
+
+        socket.on('disconnect', () => {
+            socket.to(roomId).broadcast.emit('user-disconneted', userId)
+        })
     })
 })
 server.listen(3000)
