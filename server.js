92effc2878445d30020a4e896f256286db39a734
szareian <szareian@sfu.ca>
2020-09-24 21:01:15 -0700

    Minor changes

--- server.js
+++ server.js
@@ -55,7 +55,6 @@ io.on('connection', socket => {
             }
 
             socket.on('send-chat-message', message => {
-                // console.log(message);
                 socket.broadcast.emit('chat-message', message);
             })
 
